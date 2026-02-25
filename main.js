cbb80b072b6860b94c4b51f0f7fbd536bdcd0767
Harrison Black <harrisonblack@duck.com>
2026-02-25 22:42:40 +1300

    Added WebRTC data channel for messaging and a fair amount of refactoring...

--- main.js
+++ main.js
@@ -3,34 +3,41 @@
 // Global vars
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
+const localMessageArea = document.getElementById('localMessage');
+const remoteMessageArea = document.getElementById('remoteMessage');
 const startCallButton = document.getElementById('startCallButton');
 const endCallButton = document.getElementById('endCallButton');
+const sendMessageButton = document.getElementById('sendMessageButton');
+let localDataChannel = null;
+let localMediaStream = null;
+
 
 async function startLocalVideo() {
     try {
+        console.log('Starting local video...');
+        
         const mediaStreamConstraints = {
             video: true, // Enable video
             audio: true, // Enable mic
         }
-
+        
         // Ask user for permission and get stream to local webcam.
-        const mediaStream = await navigator.mediaDevices.getUserMedia(mediaStreamConstraints);
-        localVideo.srcObject = mediaStream; // Render in-page video
-
-        return mediaStream;
+        localMediaStream = await navigator.mediaDevices.getUserMedia(mediaStreamConstraints);
+        
+        localVideo.srcObject = localMediaStream; // Render in-page video
     } catch(err) {
-        console.log('User blocked media stream or something went wrong', err);
+        console.error('Something went wrong start local video media stream')
     }
-
-    return null;
 }
 
 function stopLocalVideo() {
+    console.log('Stopping local video...');
+
     // Release camera + mic resources by stopping media stream tracks
     localVideo.srcObject?.getTracks().forEach(track => track.stop());
 }
 
-function handleTransportedLocalVideo(event) {
+function receiveMediaStreamCallback(event) {
     console.log('Receiving local peer video...');
 
     // Apply media stream to remove video
@@ -40,7 +47,7 @@ function handleTransportedLocalVideo(event) {
 async function findIceCandidates(event, otherPeerConnection) {
     // ICE networking technique to find best path to connect peers
     // Generally requires STUN server due to NATs
-    // const peerConnection = event.target; // For reference
+    // const peerConnection = event.target; // Can find RTCPeerConnection this way
     const iceCandidate = event.candidate;
 
     console.log(`Attempting to add ICE candidate ${iceCandidate?.candidate} with address ${iceCandidate?.address} to ${otherPeerConnection.name}`);
@@ -56,8 +63,12 @@ async function findIceCandidates(event, otherPeerConnection) {
     }
 }
 
-async function exchangeMediaMetadata(localPeerConnection, remotePeerConnection) {
+async function exchangeMetadata(localPeerConnection, remotePeerConnection) {
     try {
+        // Upon ICE candidate being identified
+        localPeerConnection.onicecandidate = event => findIceCandidates(event, remotePeerConnection);
+        remotePeerConnection.onicecandidate = event => findIceCandidates(event, localPeerConnection);
+
         // Exchange media metadata between peers (resolution, codec etc)
         const offer = await localPeerConnection.createOffer();
         console.log(`Offer created ${offer.sdp}`);
@@ -71,42 +82,62 @@ async function exchangeMediaMetadata(localPeerConnection, remotePeerConnection) 
         await localPeerConnection.setRemoteDescription(answer);
         await remotePeerConnection.setLocalDescription(answer);
     } catch(err) {
-        console.error('Something went wrong exchanging media metadata', err);
+        console.error('Something went wrong exchanging metadata', err);
     }
 }
 
-async function startPeerConnection(localMediaStream) {
+function receiveDataChannelValue(event) {
+    console.log('Receiving local data channel...');
+    const channel = event.channel;
+
+    // Upon message received from data channel assign to peer text area
+    channel.onmessage = event => remoteMessageArea.value = event.data;
+}
+
+
+async function startPeerConnection() {
+    console.log('Starting peer->peer connection...');
+
+    await startLocalVideo(); // Pre-requisite before below logic
+
     const servers = null; // STUN/TURN server config
 
-    console.log('Starting peer-peer connection...');
+    // For SCTP (WebRTC data channel protocol), reliable and ordered delivery 
+    // is true by default.
     const localPeerConnection = new RTCPeerConnection(servers);
     const remotePeerConnection = new RTCPeerConnection(servers);
     
-    localPeerConnection.name = localPeerConnection.name ?? 'localPeerConnection';
-    remotePeerConnection.name = remotePeerConnection.name ?? 'RemotePeerConnection';
+    localPeerConnection.name = localPeerConnection.name ?? 'local peer conection';
+    remotePeerConnection.name = remotePeerConnection.name ?? 'remote peer connection';
 
-    // Add local video tracks to be streamed to receiver
-    localMediaStream.getTracks().forEach(
+    // Stream media stream to receiver
+    // Media stream must be attached before metadata exchange!
+    localMediaStream?.getTracks().forEach(
         track => localPeerConnection.addTrack(track, localMediaStream)
     );
 
     // Upon sender (local peer) adding tracks
-    remotePeerConnection.ontrack = handleTransportedLocalVideo;
+    remotePeerConnection.ontrack = receiveMediaStreamCallback;
     
-    localPeerConnection.onicecandidate = event => findIceCandidates(event, remotePeerConnection);
-    remotePeerConnection.onicecandidate = event => findIceCandidates(event, localPeerConnection);
+    // Create data channel for messaging.
+    // Must be created before metadata exchange!
+    localDataChannel = localPeerConnection.createDataChannel('message_channel');
+    remotePeerConnection.ondatachannel = receiveDataChannelValue; // Upon data being sent
 
-    await exchangeMediaMetadata(localPeerConnection, remotePeerConnection);
+    await exchangeMetadata(localPeerConnection, remotePeerConnection);
+
+    sendMessageButton.disabled = false; // Messages on data channel can now be sent
+    console.log('Peer->peer initiation steps invoked');
 }
 
 async function startCall() {
+    console.log('Starting call...');
+
     // Invert allowed actions
     startCallButton.disabled = true;
     endCallButton.disabled = false;
-
-    console.log('Starting call...');
-    const mediaStream = await startLocalVideo();
-    await startPeerConnection(mediaStream);
+    
+    await startPeerConnection();
 
     console.log('Call started');
 }
@@ -120,14 +151,20 @@ async function endCall() {
 
     stopLocalVideo();
 
-    // Remove video from page (will still show last frame after camera stopped)
+    // Remove video from page (will still show last frame after stopLocalVideo())
     localVideo.srcObject = null;
     remoteVideo.srcObject = null;
 
     console.log('Call ended');
 }
 
+function sendLocalMessage() {
+    console.log('Sending message on local data channel...');
+    localDataChannel.send(localMessageArea.value);
+}
+
 startCallButton.onclick = startCall;
 endCallButton.disabled = true; // Can't end call until you start it
 endCallButton.onclick = endCall;
-
+sendMessageButton.disabled = true; // Dependent on starting call for peer connection...
+sendMessageButton.onclick = sendLocalMessage;
